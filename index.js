9c8a893219392509c6923f823d475ecd287c3672
Sigolletes <juditreverter87@gmail.com>
2023-01-30 12:03:05 +0100

    uibuild

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const cors = require('cors')
 const Person = require('./models/person')
 
-let persons = [
+/* let persons = [
   { 
     "id": 1,
     "name": "Arto Hellas", 
@@ -25,7 +25,7 @@ let persons = [
     "name": "Mary Poppendieck", 
     "number": "39-23-6423122"
   }
-]
+] */
 
 app.use(express.json())
 
